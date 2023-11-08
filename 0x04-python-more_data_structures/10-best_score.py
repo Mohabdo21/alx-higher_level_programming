73aefbae01d9dd83d4fdef55ee11e7221c633a4e
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-11-08 20:47:11 +0400

    10. Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_score = max(a_dictionary.values())
-        best_student = [key for key, value in a_dictionary.items() if value == max_score][0]
+        best_student = [key for key, value in a_dictionary.items()
+                        if value == max_score][0]
         return best_student
     return None
