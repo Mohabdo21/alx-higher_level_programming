f9bdd5bd0b06df6087d2efc852cc27a216b1f245
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-11-22 00:18:04 +0400

    4. Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,6 +7,8 @@ class Square:
     """
     def __init__(self, size=0):
         """Initialize a new Square with a size.
+        Args:
+            size (int): The size of the new square.
         """
         self.size = size
 
