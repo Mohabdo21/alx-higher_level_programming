032f5fa97f301de559d43f3bbbdc1dc6030bb50d
Mohabdo21 <mohannadabdo21@hotmail.com>
2023-12-05 01:09:59 +0400

    7. Integer validator

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -38,5 +38,5 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        if value > 10**6:
+        if value > 10**20:
             raise ValueError("{} must be greater than 0".format(name))
