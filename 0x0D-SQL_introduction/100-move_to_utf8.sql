1d98518496ee6f193644ccb2e6cc6a883e313c96
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-01-17 01:57:14 +0400

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -13,6 +13,6 @@ COLLATE utf8mb4_unicode_ci;
 
 -- Converts the name field in first_table to UTF8
 ALTER TABLE first_table
-MODIFY name VARCHAR(256)
+CHANGE name name VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
