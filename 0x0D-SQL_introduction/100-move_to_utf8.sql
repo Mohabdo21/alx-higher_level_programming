05fa591bbb850ab03b7b38382787b2cc63d0f279
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-01-17 01:53:55 +0400

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -3,6 +3,9 @@ ALTER DATABASE hbtn_0c_0
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
+-- Select hbtn_0c_0 database
+USE hbtn_0c_0
+
 -- Converts the first_table table to UTF8
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
