78981d8289d7f45d388267c4724262e6121cc752
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-01-18 01:47:43 +0400

    18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,9 +1,17 @@
 -- lists all shows without the genre Comedy
-SELECT title 
-FROM tv_shows 
-WHERE id NOT IN (
-    SELECT show_id 
-    FROM tv_show_genres 
-    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
+SELECT DISTINCT title
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title NOT IN (
+    SELECT title
+    FROM tv_shows
+    INNER JOIN tv_show_genres
+    ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_genres.name = "Comedy"
 )
 ORDER BY title;
