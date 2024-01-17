ef4ac1d2c899e29e53b1b014f30877fbba2d9f5b
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-01-18 00:32:31 +0400

    6. States table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- Create database hbtn_0d_usa if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+-- Create table states if it does not exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
