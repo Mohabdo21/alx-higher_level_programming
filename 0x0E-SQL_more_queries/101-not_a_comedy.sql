cb9bebae237ae2c673bd7043fb5ad6401ee6d393
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-01-18 01:42:35 +0400

    18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,10 +4,6 @@ FROM tv_shows
 WHERE id NOT IN (
     SELECT show_id 
     FROM tv_show_genres 
-    WHERE genre_id = (
-        SELECT id 
-        FROM tv_genres 
-        WHERE name = 'Comedy'
-    )
+    WHERE genre_id = (SELECT id FROM tv_genres WHERE name = 'Comedy')
 )
 ORDER BY title;
