532e2819529d52b38645545751db84674d6a79e4
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-02-12 23:09:55 +0400

    Fix condition

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const arg = process.argv[2];
 const num = parseInt(arg);
 
-if (isNaN(num) || num < 1) {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < num; i++) {
