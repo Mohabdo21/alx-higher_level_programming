893d3622f334a6b4814230c35fdbdca8c5f0c509
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-02-13 22:45:59 +0400

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class NewSquare extends Square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = NewSquare;
