72e3238de64f2e1d2a064a64fa85b56e4b9fab03
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-02-17 02:14:09 +0400

    For task 16. List relationship: - Fetche all the records at once - Use 4 Spaces insteade of

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -28,9 +28,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print("    {}: {}".format(city.id, city.name))
 
     session.close()
