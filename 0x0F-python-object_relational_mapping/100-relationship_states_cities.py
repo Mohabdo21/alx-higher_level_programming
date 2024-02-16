e649a5b415ebdb007181564b95619f9b952cc836
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-02-16 16:27:31 +0400

    Fix code style on #100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,17 +13,20 @@ from relationship_city import City
 from relationship_state import Base, State
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
+            username,
+            password,
+            database
+            ),
         pool_pre_ping=True,
     )
 
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
     session.add(City(name="San Francisco", state=State(name="California")))
