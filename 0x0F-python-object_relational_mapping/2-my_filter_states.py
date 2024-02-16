1f565cd0b827651c9e4253eef1506b25d4364a00
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-02-16 15:48:51 +0400

    Use format in #2 pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,12 +12,18 @@ if __name__ == "__main__":
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=database
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
     )
 
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            state_name
+            )
     )
     rows = cursor.fetchall()
 
