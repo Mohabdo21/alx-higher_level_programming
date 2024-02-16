51ee428049f3111385fe22914b0d5d985f128da2
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-02-16 16:02:04 +0400

    for task #2 make it case-sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC".format(
             state_name
             )
     )
