40b3aa9f5eaff80012ae454238751fab21669b3c
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-02 01:43:23 +0400

    8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ http://0.0.0.0:5000/search_user with the letter as a parameter
 
 import sys
 
-import request
+import requests
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         q = ""
 
     payload = {"q": q}
-    response = request.post("http://0.0.0.0:5000/search_user", data=payload)
+    response = requests.post("http://0.0.0.0:5000/search_user", data=payload)
 
     try:
         json_response = response.json()
