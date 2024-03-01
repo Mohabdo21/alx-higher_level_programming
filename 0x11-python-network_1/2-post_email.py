e5291b6374d1d03b27974dadb070e58fb8b63316
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-02 01:03:32 +0400

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script takes a URL and an email, sends a POST req to the URL
+with the email as a parameter, then displays the response body.
+"""
+
+
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    values = {"email": email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode("ascii")
+
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
