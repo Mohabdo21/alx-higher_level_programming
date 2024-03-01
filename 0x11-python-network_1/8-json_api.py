94250ae4eb7a90de9469ffd084a505db29e3e854
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-02 01:41:50 +0400

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python script takes in a letter and sends a POST request to:
+http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+
+import sys
+
+import request
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    payload = {"q": q}
+    response = request.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
