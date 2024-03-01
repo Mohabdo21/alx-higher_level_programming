021d34032d153677de3fee76635dfd730523554e
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-03-02 01:15:42 +0400

    4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Python script fetches https://alx-intranet.hbtn.io/status
+"""
+
+
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
