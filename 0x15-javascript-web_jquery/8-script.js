4e5156d83f0ab49597051714e9f587987aa81eaa
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-04 01:46:04 +0400

    8. Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,11 @@
+/* global $ */
+$(document).ready(function () {
+  $.get(
+    'https://swapi-api.alx-tools.com/api/films/?format=json',
+    function (data) {
+      for (let i = 0; i < data.results.length; i++) {
+        $('#list_movies').append('<li>' + data.results[i].title + '</li>');
+      }
+    }
+  );
+});
