6ec0d4ac8834eda4ec47d6dbf9853606306a60bd
Mohabdo21 <mohannadabdo21@hotmail.com>
2024-04-04 01:22:18 +0400

    4. Toggle classes

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+/* global $ */
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('green')) {
+      $('header').removeClass('green').addClass('red');
+    } else {
+      $('header').removeClass('red').addClass('green');
+    }
+  });
+});
